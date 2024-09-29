1b350ed4bb8af42ce50956b0b02b80b6c3925f84
Jerry Zhang <jerryfish@163.com>
2024-09-29 14:33:43 +1300

    8 - Signing Users Up
    feat:
    
    - 8 - Signing Users Up
    
    - Solve firebase building issue on iOS

--- lib/services/auth_service.dart
+++ lib/services/auth_service.dart
@@ -1,5 +1,33 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter_auth_tut/models/app_user.dart';
 
 class AuthService {
   static final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
+
+  // sign up a new user
+  static Future<AppUser?> signUp(String email, String password) async {
+    try {
+      final UserCredential credential = await _firebaseAuth
+          .createUserWithEmailAndPassword(email: email, password: password);
+      final User? user = credential.user;
+
+      if (user != null) {
+        return AppUser(uid: user.uid, email: user.email!);
+      }
+
+      return null;
+    } catch (e) {
+      return null;
+    }
+
+    // try {
+    //   final UserCredential credential = await _firebaseAuth
+    //       .createUserWithEmailAndPassword(email: email, password: password);
+    //   final User? user = credential.user;
+    //   return AppUser(uid: user!.uid, email: user.email!);
+    // } on FirebaseAuthException catch (e) {
+    //   print('Failed to sign up: ${e.message}');
+    //   return null;
+    // }
+  }
 }
