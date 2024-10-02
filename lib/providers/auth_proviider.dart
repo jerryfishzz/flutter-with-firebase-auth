33c4f83432338f8d6679c3629d8961f8ce317470
Jerry Zhang <jerryfish@163.com>
2024-10-02 16:31:04 +1300

    13 - Auth State Changes
    feat: 13 - Auth State Changes

--- lib/providers/auth_proviider.dart
+++ lib/providers/auth_proviider.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_auth_tut/models/app_user.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -5,4 +6,21 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 // ref argument is a ProviderReference that allows you to read other providers.
 // * means that the function is a generator function,
 // returning several values over time, but not just one.
-final authProvider = StreamProvider.autoDispose<AppUser?>((ref) async* {});
+final authProvider = StreamProvider.autoDispose<AppUser?>((ref) async* {
+  // create a stream that provides continues values (user/null)
+  final Stream<AppUser?> userStream =
+      FirebaseAuth.instance.authStateChanges().map((User? user) {
+    if (user == null) {
+      return null;
+    }
+    return AppUser(
+      uid: user.uid,
+      email: user.email!,
+    );
+  });
+
+  // YIELD that value whenever it changes
+  await for (final AppUser? user in userStream) {
+    yield user;
+  }
+});
